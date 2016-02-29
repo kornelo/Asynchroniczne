32e3dee4d94d8f64c188cacaa2dd0a850711c79a
ostrowskik <dudeostrowski@gmail.com>
2016-02-29 22:59:35 +0100

    asynchroniczne-oor-cw4

---
+++ Programowanie_ asynchroniczne/Programowanie_ asynchroniczne/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Programowanie__asynchroniczne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_uruchom = new System.Windows.Forms.Button();
+            this.lbl_napis = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_uruchom
+            // 
+            this.btn_uruchom.Location = new System.Drawing.Point(103, 263);
+            this.btn_uruchom.Name = "btn_uruchom";
+            this.btn_uruchom.Size = new System.Drawing.Size(111, 23);
+            this.btn_uruchom.TabIndex = 0;
+            this.btn_uruchom.Text = "Uruchom zadanie";
+            this.btn_uruchom.UseVisualStyleBackColor = true;
+            this.btn_uruchom.Click += new System.EventHandler(this.btn_uruchom_Click);
+            // 
+            // lbl_napis
+            // 
+            this.lbl_napis.AutoSize = true;
+            this.lbl_napis.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_napis.Location = new System.Drawing.Point(48, 105);
+            this.lbl_napis.Name = "lbl_napis";
+            this.lbl_napis.Size = new System.Drawing.Size(14, 24);
+            this.lbl_napis.TabIndex = 1;
+            this.lbl_napis.Text = "l";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 298);
+            this.Controls.Add(this.lbl_napis);
+            this.Controls.Add(this.btn_uruchom);
+            this.Name = "Form1";
+            this.Text = "Programowanie asynchroniczne";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_uruchom;
+        private System.Windows.Forms.Label lbl_napis;
+    }
+}
+
---
+++ Programowanie_ asynchroniczne/Programowanie_ asynchroniczne/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programowanie__asynchroniczne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_uruchom_Click(object sender, EventArgs e)
+        {
+            CallBigImportantMethod();
+            lbl_napis.Text = "Czekaj, trochę to trwa...";
+        }
+
+        private async void CallBigImportantMethod()
+        {
+            var result = await BigLongImportantMethodAsync(" udało się wykonać");
+            lbl_napis.Text = result;
+        }
+
+        private Task<string> BigLongImportantMethodAsync(string name)
+        {
+            return Task.Factory.StartNew(() => BigLongImportantMethod(name));
+        }
+
+        private string BigLongImportantMethod(string name)
+        {
+            Thread.Sleep(10000);
+            return "Witaj " + name;
+        }
+    }
+}
